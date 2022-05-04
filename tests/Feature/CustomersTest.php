3d3e7756eaf20fc57a354f3df3d9cf3a33e1361b
Жугинский Иван <Zhuga200117@gmail.com>
2022-05-04 14:10:07 +0300

    test: :white_check_mark: Покрыл тестами ендпоинт customers

--- tests/Feature/CustomersTest.php
+++ tests/Feature/CustomersTest.php
@@ -2,20 +2,101 @@
 
 namespace Tests\Feature;
 
-use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use function Pest\Laravel\seed;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Testing\Fluent\AssertableJson;
 
 uses(RefreshDatabase::class);
 
 uses()->group('customers');
 
 beforeEach(function () {
-    $seeder = new DatabaseSeeder();
-    $seeder->run();
+    Artisan::call('migrate');
+    Artisan::call('db:seed');
 });
 
 test('Can get customers', function () {
     $responce = $this->getJson('/api/v1/customers');
-    $responce->assertStatus(200);
+    $responce->assertStatus(200)
+        ->assertJson(fn (AssertableJson $json) =>
+        $json->has('data')
+            ->has('data.0', fn (AssertableJson $json) =>
+            $json->has('id')->has('name'))->has('meta')
+        );
+});
+
+test('Can get customers with addresses', function () {
+    $responce = $this->getJson('/api/v1/customers?include=addresses');
+    $responce->assertStatus(200)
+        ->assertJson(fn (AssertableJson $json) =>
+        $json->has('data')
+            ->has('data.0', fn (AssertableJson $json) =>
+            $json->has('id')
+                ->has('name')
+                ->has('addresses'))->has('meta')
+        );
+});
+
+test('Can get customers with orders', function () {
+    $responce = $this->getJson('/api/v1/customers?include=orders');
+    $responce->assertStatus(200)
+        ->assertJson(fn (AssertableJson $json) =>
+        $json->has('data')
+            ->has('data.0', fn (AssertableJson $json) =>
+            $json->has('id')
+                ->has('name')
+                ->has('orders'))->has('meta')
+        );
+});
+
+test('Can get customers with orders and addresses', function () {
+    $responce = $this->getJson('/api/v1/customers?include=orders, addresses');
+    $responce->assertStatus(200)
+        ->assertJson(fn (AssertableJson $json) =>
+        $json->has('data')
+            ->has('data.0', fn (AssertableJson $json) =>
+            $json->has('id')
+                ->has('name')
+                ->has('orders')
+                ->has('addresses'))->has('meta')
+        );
+});
+
+test('Can get customers with addresses and orders', function () {
+    $responce = $this->getJson('/api/v1/customers?include=addresses, orders');
+    $responce->assertStatus(200)
+        ->assertJson(fn (AssertableJson $json) =>
+        $json->has('data')
+            ->has('data.0', fn (AssertableJson $json) =>
+            $json->has('id')
+                ->has('name')
+                ->has('orders')
+                ->has('addresses'))->has('meta')
+        );
+});
+
+test("Can't get customers with invalid include", function () {
+    $responce = $this->getJson('/api/v1/customers?include=dwadas');
+    $responce->assertStatus(400)
+        ->assertJson(fn (AssertableJson $json) =>
+        $json->where('data', null)
+            ->has('errors')
+            ->has('errors.0', fn (AssertableJson $json) =>
+            $json->has('code')
+                ->has('message')
+                ->has('meta')));
+});
+
+test("Internal server error", function () {
+    Artisan::call('migrate:rollback');
+    $responce = $this->getJson('/api/v1/customers');
+    $responce->dump();
+    $responce->assertStatus(500)
+        ->assertJson(fn (AssertableJson $json) =>
+        $json->where('data', null)
+            ->has('errors')
+            ->has('errors.0', fn (AssertableJson $json) =>
+            $json->has('code')
+                ->has('message')
+                ->has('meta')));
 });
